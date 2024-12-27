eb716866f4e4d6a565cd5412ffd90f03f7db087b
mohan <mohan.baskaran1998@gmail.com>
2024-12-27 21:07:16 +0530

    Revert "Delete Core_Java_Spring_Boot directory"
    This reverts commit 89dbbb1e9744e82d0d5bc75170bf61ab2a09ad11.

---
+++ Core_Java_Spring_Boot/src/test/java/com/mohan/core_java_spring_boot/CoreJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohan.core_java_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
